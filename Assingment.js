0372de9c1522aae4c4a77c32163cd61cdc0ebf46
Munna Hossain <mayinuddin.munna@gmail.com>
2021-01-21 23:42:29 +0600

    km to meter converted

---
+++ Assingment.js
@@ -0,0 +1,12 @@
+
+function kilometerToMeter(number) {
+    //let storeNumber = number * 10 * 100;
+    for (let i = 1; i <= number; i++) {
+        //let convert = number * 10 * 100;
+    }
+    let storeNumber = number * 10 * 100;
+
+    return storeNumber;
+}
+let result = kilometerToMeter(1);
+console.log(result);
