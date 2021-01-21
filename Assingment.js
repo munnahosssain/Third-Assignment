30a269888c475dbc31d405697d7d7ead0c9aacbe
Munna Hossain <mayinuddin.munna@gmail.com>
2021-01-22 01:39:30 +0600

    km to m complete

--- Assingment.js
+++ Assingment.js
@@ -1,12 +1,22 @@
-
+/*Github file url::  https://github.com/munnahosssain/Third-Assignment*/
 function kilometerToMeter(number) {
-    //let storeNumber = number * 10 * 100;
-    for (let i = 1; i <= number; i++) {
-        //let convert = number * 10 * 100;
+    let duplicateNum = number;
+    if (number <= 0) {
+        console.log("Warning!, You should be positive number.");
+    }
+    else if (number == 1) {
+        console.log(duplicateNum, "Kilometer is equal to: ");
+    }
+    else {
+        console.log(duplicateNum, "Kilometers is equal to: ");
     }
     let storeNumber = number * 10 * 100;
-
     return storeNumber;
 }
-let result = kilometerToMeter(1);
-console.log(result);
+let result = kilometerToMeter(110);//Pre define user input
+if (result <= 0) {
+    console.log();
+}
+else {
+    console.log(result, " Meters");
+}
